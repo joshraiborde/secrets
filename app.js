c5847623def844d755fb80dcfeb5e79f6a5be334
joshraiborde <joshraiborde@gmail.com>
2022-04-29 18:13:28 -0400

    added app.post to the register route so that registed and logged in users can view secrets

--- app.js
+++ app.js
@@ -20,15 +20,14 @@ mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
 // create a new schema called userSchema
 const userSchema = {
-    email: String,
-    password: String
+  email: String,
+  password: String
 };
 
 // create a new mongoose model called User, based on the userSchema
 // collection name is "User"
 // specify the schema, userSchema
 const user = new mongoose.model("User", userSchema);
-
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -40,6 +39,21 @@ app.get("/login", (req, res) => {
 
 app.get("/register", (req, res) => {
   res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets"); // you can only see the secrets if you are registed and logged in
+    }
+  });
 });
 
 app.listen(port, () => {
