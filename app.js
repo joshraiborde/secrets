cf8fe75f0077d867f83d38cdc6758376d3494cca
joshraiborde <joshraiborde@gmail.com>
2022-04-29 18:01:38 -0400

    connected to mongoose, created userSchema, and user model

--- app.js
+++ app.js
@@ -15,6 +15,21 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// CONNECT to MONGOOSE, create a new database called userDB
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+
+// create a new schema called userSchema
+const userSchema = {
+    email: String,
+    password: String
+};
+
+// create a new mongoose model called User, based on the userSchema
+// collection name is "User"
+// specify the schema, userSchema
+const user = new mongoose.model("User", userSchema);
+
+
 app.get("/", (req, res) => {
   res.render("home");
 });
