5cd16da6653a42252e6ab3bccaf22a56fed8e6cd
joshraiborde <joshraiborde@gmail.com>
2022-04-29 17:30:55 -0400

    added app.get() to home, login, and register

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //jshint esversion:6
 
 const express = require("express");
-const bodyparser = require("body-parser");
+const bodyParser = require("body-parser");
 const ejs = require("ejs");
 require("dotenv").config()
 
@@ -14,7 +14,17 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+    res.render("home");
+});
 
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
 
 
 app.listen(port, () => {
