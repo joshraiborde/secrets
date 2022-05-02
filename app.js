a93e1bd13c0e569bcc6620db41d2696553d12de8
joshraiborde <joshraiborde@gmail.com>
2022-05-02 18:21:46 -0400

    installed md5 and refactor the code to meet md5 specs

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require('md5');
+// const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -27,8 +28,8 @@ const userSchema = new mongoose.Schema ({
 });
 
 // define a secret
-const secret = process.env.SECRET
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+// const secret = process.env.SECRET
+// userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 //verify that this plugin is added before you create a mongoose model
 // only the password is encrypted
 
@@ -52,7 +53,7 @@ app.get("/register", (req, res) => {
 app.post("/register", (req, res) => {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save((err) => {
