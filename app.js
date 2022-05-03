9cfdde20545b079cb8c90c790f816f4d0584af65
joshraiborde <joshraiborde@gmail.com>
2022-05-03 17:28:53 -0400

    installed bcrypt and refactored code for bcrypt specs

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require('md5');
+const bcrypt = require('bcrypt')
+const saltRounds = 10; // as recommended in the dox.
+// const md5 = require('md5');
 // const encrypt = require("mongoose-encryption");
 
 const app = express();
@@ -51,23 +53,26 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-  });
 
-  newUser.save((err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets"); // you can only see the secrets if you are registed and logged in
-    }
-  });
+  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+  
+    newUser.save((err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets"); // you can only see the secrets if you are registed and logged in
+      }
+    });
+});
 });
 
 app.post("/login", (req, res) => {
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({ email: username }, (err, foundUser) => {
     if (err) {
@@ -75,10 +80,12 @@ app.post("/login", (req, res) => {
     } else {
       if (foundUser) {
         //if the user if found in the databse, do the following
-        if (foundUser.password === password) {
-          //if the password typed in at login matches the password in the database (foundUser.password) do the following
-          res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, (err, result) => {
+          if (result == true) {
+            res.render("secrets");
+            //if the password typed in at login matches the password in the database (foundUser.password) do the following
+          }
+      });
       }
     }
   });
