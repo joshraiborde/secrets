ef3ce3238b12916e46ee995f652ac9d627ee657d
joshraiborde <joshraiborde@gmail.com>
2022-05-04 10:39:25 -0400

    removed all the bcrypt and saltRounds components and emptied the code blocks of app.post login and register

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require('bcrypt')
-const saltRounds = 10; // as recommended in the dox.
+// const bcrypt = require('bcrypt')
+// const saltRounds = 10; // as recommended in the dox.
 // const md5 = require('md5');
 // const encrypt = require("mongoose-encryption");
 
@@ -54,41 +54,11 @@ app.get("/register", (req, res) => {
 
 app.post("/register", (req, res) => {
 
-  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-    const newUser = new User({
-      email: req.body.username,
-      password: hash
-    });
   
-    newUser.save((err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("secrets"); // you can only see the secrets if you are registed and logged in
-      }
-    });
-});
 });
 
 app.post("/login", (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  User.findOne({ email: username }, (err, foundUser) => {
-    if (err) {
-      console.log(err);
-    } else {
-      if (foundUser) {
-        //if the user if found in the databse, do the following
-        bcrypt.compare(password, foundUser.password, (err, result) => {
-          if (result == true) {
-            res.render("secrets");
-            //if the password typed in at login matches the password in the database (foundUser.password) do the following
-          }
-      });
-      }
-    }
-  });
+  
 });
 
 app.listen(port, () => {
