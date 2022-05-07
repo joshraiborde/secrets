1e9296bd03143b1fb93fb0b850d49402bfeb3777
joshraiborde <joshraiborde@gmail.com>
2022-05-07 13:37:32 -0400

    troubleshooting InternalOAuthError: Failed to fetch user profile

--- app.js
+++ app.js
@@ -42,7 +42,8 @@ mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 // create a new schema called userSchema
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  googleId: String
 });
 
 // this will hash and salt the passwords and save the users in MongoDB userDB database
@@ -58,17 +59,29 @@ const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/" + process.env.CALLBACKURL
+      callbackURL: "http://localhost:3000/" + process.env.CALLBACKURL,
+      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
     },
     function (accessToken, refreshToken, profile, cb) {
+      console.log(profile);
       User.findOrCreate({ googleId: profile.id }, function (err, user) {
         return cb(err, user);
       });
@@ -79,6 +92,22 @@ passport.use(
 app.get("/", (req, res) => {
   res.render("home");
 });
+
+// use passport to authenticate the google strategy, needing their google profile, including thier user id
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+
+// this should allow a google user id to login and see the secrets page.
+app.get(
+  "/auth/google/secrets",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  (req, res) => {
+    // Successful authentication, redirect to secrets.
+    res.redirect("/secrets");
+  }
+);
 
 app.get("/login", (req, res) => {
   res.render("login");
